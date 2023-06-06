6ce0993d1f68e5b73fc7ef1b1a4136e50486d848
ihsajjad <sajjadmeal@gmail.com>
2023-06-07 00:23:28 +0600

    created 404 page

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/home/home/Home";
+import NotFound from "../pages/404/NotFound";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Main />,
+        errorElement: <NotFound />,
         children: [
             {
                 path: '/',
