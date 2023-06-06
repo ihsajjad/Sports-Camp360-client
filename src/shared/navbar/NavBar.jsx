bd863303af79a9371953896d8b534522ea95b2ea
ihsajjad <sajjadmeal@gmail.com>
2023-06-06 23:11:00 +0600

    added responsive navbar

---
+++ src/shared/navbar/NavBar.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+
+const NavBar = () => {
+
+    const menuItems = <>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to=""></Link></li>
+        <li><Link to=""></Link></li>
+    </>
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <Link to="/" className="text-2xl font-bold text-slate-600">Sports<span className="sc-color">Camp</span>360</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {menuItems}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
