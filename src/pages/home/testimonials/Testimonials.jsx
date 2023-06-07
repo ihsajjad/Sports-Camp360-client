4f136b992ba529b037650f7aae24ece045171e90
ihsajjad <sajjadmeal@gmail.com>
2023-06-08 01:06:28 +0600

    added data to testimonials

--- src/pages/home/testimonials/Testimonials.jsx
+++ src/pages/home/testimonials/Testimonials.jsx
@@ -1,23 +1,44 @@
 import SectionTitle from "../../../components/SectionTitle";
+import { FaStar } from "react-icons/fa";
 
 
 const Testimonials = () => {
+    const testimonials = [
+        {
+            name: "Emma Johnson",
+            img: "https://rb.gy/8q7wm",
+            description: "Sports Camp 360 has been an incredible experience for me! I have learned so much and made lifelong friendships. Highly recommended!",
+        },
+        {
+            name: "Daniel Rodriguez",
+            img: "https://rb.gy/59p4j",
+            description: "Attending Sports Camp 360 was the best decision I ever made. The coaches are exceptional, and the training has taken my skills to the next level.",
+        },
+        {
+            name: "Sophia Chen",
+            img: "https://rb.gy/tyefa",
+            description: "Sports Camp 360 provided me with opportunities to explore various sports and discover my true passion. It's been an unforgettable journey!",
+        },
+    ];
+
     return (
         <div className="md:py-12">
             <SectionTitle title={"Testimonials"} subTitle={"Hear From Beginner to Champion, Our Students Share Their Stories!"}> </SectionTitle>
-            <div className="bg-[#fb00d993]">
-                <div className=" border border-[#fb00d993] rounded-lg">
-                    <div className="h-60  rounded-bl-3xl">
-                        <img src={"instructor.image"} alt="car!" className="h-28 w-28 rounded-full" />
-                    </div>
-                    <div className="p-4 space-y-2 flex flex-col bg-white rounded-tr-3xl">
-                        <h2 className="card-title">{"name"}</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, ratione quas cum accusamus repellendus neque facilis! Adipisci non quis cum.</p>
-                        <div className="card-actions justify-end mt-auto">
-                            <button className="custom-btn-outline">See Classes</button>
+            <div className="grid md:grid-cols-3 grid-cols-1 gap-8 p-5 md:px-8">
+                {
+                    testimonials.map((testimonial, i) => <div key={i} className=" border border-[#fb00d993] rounded-lg flex flex-col items-center space-y-2 p-5 text-center">
+                        <img src={testimonial.img} alt="car!" className="h-28 w-28 rounded-full border-2 border-[#fb00d993]" />
+                        <h2 className="card-title">{testimonial.name}</h2>
+                        <p>{testimonial.description}</p>
+                        <div className="flex gap-1">
+                            <FaStar />
+                            <FaStar />
+                            <FaStar />
+                            <FaStar />
+                            <FaStar />
                         </div>
-                    </div>
-                </div>
+                    </div>)
+                }
             </div>
         </div>
     );
