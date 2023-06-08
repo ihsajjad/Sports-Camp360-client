b1181ce3ee75083a414026333a35260dd4b02e6e
ihsajjad <sajjadmeal@gmail.com>
2023-06-09 01:18:31 +0600

    implemented spinner as loading content

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+
+
+const Spinner = () => {
+    return (
+        <button className="btn">
+            loading...<span className="loading loading-spinner"></span>
+        </button>
+    );
+};
+
+export default Spinner;
--- src/routes/PrivetRoute.jsx
+++ src/routes/PrivetRoute.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { Navigate } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 
 const PrivetRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
 
     if (loading) {
-        return <h2>Loading...</h2>
+        return <div className="min-h-screen flex items-center justify-center"><Spinner /></div>
     }
     if (user) {
         return children;
